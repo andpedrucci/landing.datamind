b7065c3cbd0f0c513eac199c2e3c120542b5742b
Anderson Pedrucci Filho <62671446+andpedrucci@users.noreply.github.com>
2025-09-15 11:32:46 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+// script.js
+
+const sections = document.querySelectorAll(".page-section");
+let current = 0; // começa na primeira seção
+let isScrolling = false;
+
+function scrollToSection(index) {
+  if (index >= 0 && index < sections.length) {
+    isScrolling = true;
+    sections[index].scrollIntoView({ behavior: "smooth" });
+    setTimeout(() => (isScrolling = false), 1000); // tempo pra evitar scroll duplo
+    current = index;
+  }
+}
+
+window.addEventListener("wheel", (e) => {
+  if (isScrolling) return;
+  if (e.deltaY > 0) {
+    // scroll para baixo
+    scrollToSection(current + 1);
+  } else {
+    // scroll para cima
+    scrollToSection(current - 1);
+  }
+});
