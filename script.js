7830d50a0be21cbcef0f9dfb52e7b769972a8623
Anderson Pedrucci Filho <62671446+andpedrucci@users.noreply.github.com>
2025-09-17 13:48:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-// Seleciona as seções e as trilhas dos circuitos
 const sections = document.querySelectorAll(".page-section");
-const traces = document.querySelectorAll(".trace");
 
-// Observa quando a seção entra em tela
+// pega todas as trilhas separadas por etapa
+const step1 = document.querySelectorAll(".step1");
+const step2 = document.querySelectorAll(".step2");
+const step3 = document.querySelectorAll(".step3");
+
 const observer = new IntersectionObserver((entries)=>{
   entries.forEach(entry=>{
     if(entry.isIntersecting){
-      // quando a seção 2 ou 3 entra em tela, aciona animação
-      if(entry.target.id === "sec2" || entry.target.id === "sec3"){
-        traces.forEach(t => t.classList.add("grow"));
+      if(entry.target.id === "sec1"){
+        step1.forEach(t => t.classList.add("active"));
+      }
+      if(entry.target.id === "sec2"){
+        step2.forEach(t => t.classList.add("active"));
+      }
+      if(entry.target.id === "sec3"){
+        step3.forEach(t => t.classList.add("active"));
       }
     }
   })
